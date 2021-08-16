d0511d71281fdf0286412964ca17fb39680164d8
DoJiSeon <djs6620@gmail.com>
2021-08-16 22:02:15 +0900

    0816 수업
    이미지 변경

---
+++ MentoMentee0816/app/src/main/java/com/bliss/csc/mentomentee0816/sub.java
@@ -0,0 +1,16 @@
+package com.bliss.csc.mentomentee0816;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class sub extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sub);
+
+
+    }
+}
